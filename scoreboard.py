6f51f0e2a519643c36ac5aec1cc1fc79d4e66f54
ibrahim67778 <ibrahim67778@email.com>
2021-02-18 12:07:01 +0800

    road crossing game

---
+++ scoreboard.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+FONT = ("Courier", 16, "normal")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.level = 1
+        self.hideturtle()
+        self.penup()
+        self.goto(-280, 270)
+        self.update_score()
+    def update_score(self):
+        self.clear()
+        self.write(f"level: {self.level}", align="left", font=FONT)
+
+
+
+    def increase_level(self):
+        self.level += 1
+        self.update_score()
+
+    def gameover(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align="center",font=FONT)
+
